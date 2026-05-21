87a079c82cfb74af887410094700004a9b43e1b9
Ivan <IVanL107@nycstudents.net>
2026-05-21 11:38:33 -0400

    Completed partB.java

--- question-1/partB.java
+++ question-1/partB.java
@@ -1,5 +1,16 @@
 public String getShortenedName(){
-  String shortened = username;
-  
-}
+  String shortened = "";
+  int i = 0;
+  while(i < username.length()) {
+    if(i + 1 < username.length && username.substring(i + 1, i + 2).equals("-")) {
+      i += 2;
+    } else {
+      shortened += usernmae.substring(i, i + 1);
+      i++
+    }
+  }
+  return shortened;
+}  
+
+
 
