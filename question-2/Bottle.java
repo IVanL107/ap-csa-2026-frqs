edbca46b2a1ecd8a035e2deba3d6a797f88c6daa
Ivan <IVanL107@nycstudents.net>
2026-05-27 19:34:12 -0400

    Update Bottle.java

--- question-2/Bottle.java
+++ question-2/Bottle.java
@@ -1,5 +1,16 @@
 public class Bottle{
+  double cap;
+  double amo;
 
-
-
+  public Bottle(double cc) {
+    cap = cc;
+    amo = cap;
+  }
+  public doubble updateAmount(double change){
+    amo -= change;
+    if(amo < (cap  * 0.25)) {
+      amount = cap;
+    }
+    return amo;
+  }
 }
