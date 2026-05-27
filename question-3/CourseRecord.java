ae2dd6f57c2f40e39f922edcb4c0e5470e78d36f
Ivan <IVanL107@nycstudents.net>
2026-05-27 19:58:11 -0400

    Update CourseRecord.java

--- question-3/CourseRecord.java
+++ question-3/CourseRecord.java
@@ -1,5 +1,11 @@
-public class CourseRecord{
-
-
-  
+public int moreHistoryThanMathAbsences() {
+  int count = 0;
+  for(CourseRecord history : historList) {
+    for(CourseRecord math : mathList) {
+      if(history.getStudentID().equals(math.getStudentID()) && history.getAbsences() > math.getAbsences()) {
+        count++
+      }
+    }
+  }
+  return count;
 }
